80ded0c29993c4166987c382f69ac8b7d43399f8
Kinder-Eggs <g234720@dac.unicamp.br>
2020-05-05 12:11:47 -0300

    Finished bishop object

--- src/Bishop.java
+++ src/Bishop.java
@@ -3,4 +3,56 @@ public class Bishop extends Piece {
     public Bishop(boolean team, int pos) {
         super(team, pos);
     }
+
+    public void move(int npos, Table table) {
+        if((index - npos) % 7 == 0) {
+            if(index > npos) {
+                for (int i = index-7; i != npos; i -= 7) {
+                    if(table.pieces[i] != null) {
+                        System.out.println("MOVEMENT WITH BISHOP NOT ALLOWED!!!");
+                        return;
+                    }
+                }
+            } else {
+                for (int i = index+7; i != npos; i += 7) {
+                    if(table.pieces[i] != null) {
+                        System.out.println("MOVEMENT WITH BISHOP NOT ALLOWED!!!");
+                        return;
+                    }
+                }
+            }
+            if (table.pieces[npos] != null) {
+                table.removePiece(npos);
+            }
+            table.pieces[npos] = table.pieces[index];
+            table.pieces[index] = null;
+            this.index = npos;
+            return;
+        }
+        if ((index - npos) % 9 == 0) {
+            if(index > npos) {
+                for (int i = index-9; i != npos; i -= 9) {
+                    if(table.pieces[i] != null) {
+                        System.out.println("MOVEMENT WITH BISHOP NOT ALLOWED!!!");
+                        return;
+                    }
+                }
+            } else {
+                for (int i = index+9; i != npos; i += 9) {
+                    if(table.pieces[i] != null) {
+                        System.out.println("MOVEMENT WITH BISHOP NOT ALLOWED!!!");
+                        return;
+                    }
+                }
+            }
+            if (table.pieces[npos] != null) {
+                table.removePiece(npos);
+            }
+            table.pieces[npos] = table.pieces[index];
+            table.pieces[index] = null;
+            this.index = npos;
+            return;
+        }
+        System.out.println("MOVEMENT WITH BISHOP NOT ALLOWED!!!");
+    }
 }
