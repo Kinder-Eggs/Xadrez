927903251b4a512faba4b80f6c8a1e44603747df
Kinder-Eggs <g234720@dac.unicamp.br>
2020-05-05 11:56:55 -0300

    Improved piece construction and finished pawn object

--- src/Pawn.java
+++ src/Pawn.java
@@ -3,4 +3,43 @@ public class Pawn extends Piece {
     public Pawn(boolean team, int pos) {
         super(team, pos);
     }
+
+    public void move(int npos, Table table) {
+        if(team) {
+            if(index + 8 == npos || index + 16 == npos) {
+                if(table.pieces[npos] == null) {
+                    table.pieces[npos] = table.pieces[index];
+                    table.pieces[index] = null;
+                    this.index = npos;
+                    return;
+                }
+            } else if(index + 7 == npos || index + 9 == npos) {
+                if(table.pieces[npos] != null) {
+                    table.removePiece(npos);
+                    table.pieces[npos] = table.pieces[index];
+                    table.pieces[index] = null;
+                    this.index = npos;
+                    return;
+                }
+            }
+        } else {
+            if(index - 8 == npos || index - 16 == npos) {
+                if(table.pieces[npos] == null) {
+                    table.pieces[npos] = table.pieces[index];
+                    table.pieces[index] = null;
+                    this.index = npos;
+                    return;
+                }
+            } else if(index -7 == npos || index - 9 == npos) {
+                if(table.pieces[npos] != null) {
+                    table.removePiece(npos);
+                    table.pieces[npos] = table.pieces[index];
+                    table.pieces[index] = null;
+                    this.index = npos;
+                    return;
+                }
+            }
+        }
+        System.out.println("MOVEMENT WITH PAWN NOT ALLOWED!!!");
+    }
 }
--- src/Piece.java
+++ src/Piece.java
@@ -1,13 +1,13 @@
 public class Piece {
-    private boolean team; // True = Black, False = White
-    private int index;
+    public boolean team; // True = Black, False = White
+    public int index;
 
     public Piece(boolean team, int index) {
         this.team = team;
         this.index = index;
     }
 
-    public void move(int pos) {
+    public void move(int pos, Table table) {
         ;
     }
 
