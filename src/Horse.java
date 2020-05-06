e198d6ccf929004f9d6658688e3986f68ae9e06d
Kinder-Eggs <g234720@dac.unicamp.br>
2020-05-06 19:37:57 -0300

    Added horse movement

--- src/Horse.java
+++ src/Horse.java
@@ -3,4 +3,15 @@ public class Horse extends Piece {
     public Horse(boolean team, int pos) {
         super(team, pos);
     }
+
+    public void move(int npos, Table table) {
+        if(index - npos == 17 || index - npos == 15 || index - npos == 10 || index - npos == -6 || index - npos == -17
+                || index - npos == -15 || index - npos == -10 || index - npos == 6) {
+            if(table.pieces[npos] == null) {
+                table.removePiece(npos);
+            }
+            table.movePiece(index, npos);
+            this.index = npos;
+        }
+    }
 }
