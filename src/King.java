4c6383fe8a27b72cd669521d2f7901b4add9acec
Kinder-Eggs <g234720@dac.unicamp.br>
2020-05-06 11:45:38 -0300

    Added king movement rules

--- src/King.java
+++ src/King.java
@@ -3,4 +3,22 @@ public class King extends Piece {
     public King(boolean team, int pos) {
         super(team, pos);
     }
+
+    public void move(int npos, Table table) {
+        if(index - npos == 7 || index - npos == 8 || index - npos == 9 || index - npos == 1 || index - npos == -7 || index - npos == -8 || index - npos == -9 || index - npos == -1 ) {
+            if(table.pieces[npos] != null) {
+                if(table.pieces[npos].team != team) {
+                    table.removePiece(npos);
+                } else {
+                    System.out.println("MOVEMENT WITH KING NOT ALLOWED!!!");
+                    return;
+                }
+            }
+            table.pieces[npos] = table.pieces[index];
+            table.pieces[index] = null;
+            this.index = npos;
+            return;
+        }
+        System.out.println("MOVEMENT WITH KING NOT ALLOWED!!!");
+    }
 }
