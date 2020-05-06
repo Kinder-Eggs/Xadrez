4bde4c0e2d993bb741a6df85909c1e242c3a430b
Kinder-Eggs <g234720@dac.unicamp.br>
2020-05-06 11:33:54 -0300

    Finished tower object

--- src/Tower.java
+++ src/Tower.java
@@ -3,4 +3,67 @@ public class Tower extends Piece {
     public Tower(boolean team, int pos) {
         super(team, pos);
     }
+
+
+    public void move(int npos, Table table) {
+        if(npos / 8 == index / 8) {  // Horizontal movement
+            if(npos > index) {
+                for (int i = index + 1; i < npos; i++) {
+                    if(table.pieces[i] != null) {
+                        System.out.println("MOVEMENT WITH TOWER NOT ALLOWED!!!");
+                        return;
+                    }
+                }
+            } else {
+                for (int i = index - 1; i > npos; i--) {
+                    if(table.pieces[i] != null) {
+                        System.out.println("MOVEMENT WITH TOWER NOT ALLOWED!!!");
+                        return;
+                    }
+                }
+            }
+            if (table.pieces[npos] != null) {
+                if (table.pieces[npos].team != team) {
+                    table.removePiece(npos);
+                } else {
+                    System.out.println("MOVEMENT WITH TOWER NOT ALLOWED!!!");
+                    return;
+                }
+            }
+            table.pieces[npos] = table.pieces[index];
+            table.pieces[index] = null;
+            this.index = npos;
+            return;
+        }else if((npos - index) % 8 == 0) {  // Vertical movement
+            if(npos > index) {  // Downwards
+                for (int i = index + 8; i < npos; i += 8) {
+                    if(table.pieces[i] != null) {
+                        System.out.println("MOVEMENT WITH TOWER NOT ALLOWED!!!");
+                        return;
+                    }
+                }
+            } else {  // Upwards
+                if(index > npos) {  // Downwards
+                    for (int i = index - 8; i > npos; i -= 8) {
+                        if (table.pieces[i] != null) {
+                            System.out.println("MOVEMENT WITH TOWER NOT ALLOWED!!!");
+                            return;
+                        }
+                    }
+                }
+            }
+            if (table.pieces[npos] != null) {
+                if (table.pieces[npos].team != team) {
+                    table.removePiece(npos);
+                } else {
+                    System.out.println("MOVEMENT WITH TOWER NOT ALLOWED!!!");
+                    return;
+                }
+            }
+            table.pieces[npos] = table.pieces[index];
+            table.pieces[index] = null;
+            this.index = npos;
+            return;
+        }
+    }
 }
